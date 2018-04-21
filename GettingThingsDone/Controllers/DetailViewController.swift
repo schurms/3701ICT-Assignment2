42cf593a2eba60ddec23dc861550c50195246ad5
schurmann <schurms@bigpond.com>
2018-04-21 17:51:39 +1000

    - Added text vis nsdate - added static cells via UITableViewController

--- GettingThingsDone/Controllers/DetailViewController.swift
+++ GettingThingsDone/Controllers/DetailViewController.swift
@@ -9,38 +9,42 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
+class DetailViewController: UITableViewController {
+    
     @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
+    
+    @IBOutlet weak var todoText: UITextField!
+    
+    
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
+            //            if let label = detailDescriptionLabel {
+            //                label.text = detail.description
+            //            }
+            if let todoTitle = todoText {
+                todoTitle.text = detail.description
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    var detailItem: NSDate? {
+    
+    var detailItem: String? {
         didSet {
             // Update the view.
             configureView()
         }
     }
-
-
+    
+    
 }
-
--- GettingThingsDone/Controllers/MasterViewController.swift
+++ GettingThingsDone/Controllers/MasterViewController.swift
@@ -10,16 +10,19 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
-
-
+    var objects = [String]()
+    var todoRow = 0
+    let todoText = "Todo Item "
+    
+    let sections: [String] = ["YET TO DO", "COMPLETED"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.leftBarButtonItem = editButtonItem
-
+        
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
         navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
@@ -27,30 +30,37 @@ class MasterViewController: UITableViewController {
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
         }
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @objc
     func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
+        //        objects.insert(NSDate(), at: 0)
+        let todoCounter = todoRow + 1
+        print(todoCounter)
+        let textToInsert = "Todo Item " + String(todoCounter)
+        print(textToInsert)
+        objects.insert(textToInsert, at: 0)
         let indexPath = IndexPath(row: 0, section: 0)
         tableView.insertRows(at: [indexPath], with: .automatic)
+        print(indexPath)
+        todoRow += 1
     }
-
+    
     // MARK: - Segues
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = objects[indexPath.row]
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
@@ -58,30 +68,40 @@ class MasterViewController: UITableViewController {
             }
         }
     }
-
+    
     // MARK: - Table View
-
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 80
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return objects.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
+        let cellReuseIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        
+        
+        let object = objects[indexPath.row]
         cell.textLabel!.text = object.description
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             objects.remove(at: indexPath.row)
@@ -90,9 +110,8 @@ class MasterViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
         }
     }
-
-    // MARK: - Moving Cells
     
+    // MARK: Moving Cells
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         return true
     }
@@ -103,6 +122,8 @@ class MasterViewController: UITableViewController {
         objects.remove(at: sourceIndexPath.row)
         objects.insert(todoToMove, at: destinationIndexPath.row)
     }
-
+    
+    
 }
+
 

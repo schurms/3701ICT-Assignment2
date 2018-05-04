650fb9d1861841372b143149d6640bf6a2353652
schurmann <schurms@bigpond.com>
2018-05-05 08:54:04 +1000

    Commit UI Tests

--- GettingThingsDoneUITests/GettingThingsDoneUITests.swift
+++ GettingThingsDoneUITests/GettingThingsDoneUITests.swift
@@ -17,12 +17,11 @@ class GettingThingsDoneUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        // Stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test.
         app.launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {

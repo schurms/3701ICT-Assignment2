ab1aa4dab195b363fecab4c212079b67a3a186ee
schurmann <schurms@bigpond.com>
2018-05-11 21:24:08 +1000

    Deleted file

---
+++ GettingThingsDone/Resources/DataManager.swift
@@ -0,0 +1,84 @@
+//
+//  DataManager.swift
+//  GettingThingsDone
+//
+//  Created by Jordan Schurmann on 11/5/18.
+//  Copyright © 2018 Jordan Schurmann. All rights reserved.
+//
+
+import Foundation
+
+public class DataManager {
+    static fileprivate func getDocumentDirectory () -> URL {
+        if let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            return url
+        }else{
+            fatalError("Unable to get document directory")
+        }
+    }
+    
+    static func save <T:Encodable> (_ object:T, with fileName:String) {
+        let url = getDocumentDirectory().appendingPathComponent(fileName, isDirectory: false)
+        
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(object)
+            if FileManager.default.fileExists(atPath: url.path) {
+                try FileManager.default.removeItem(at: url)
+            }
+            FileManager.default.createFile(atPath: url.path, contents: data, attributes: nil)
+        }catch{
+            fatalError(error.localizedDescription)
+        }
+    }
+    
+    
+    static func load <T:Decodable> (_ fileName:String, with type:T.Type) -> T {
+        let url = getDocumentDirectory().appendingPathComponent(fileName, isDirectory: false)
+        if !FileManager.default.fileExists(atPath: url.path) {
+            fatalError("File not found at path \(url.path)")
+        }
+        
+        if let data = FileManager.default.contents(atPath: url.path) {
+            do {
+                let model = try JSONDecoder().decode(type, from: data)
+                return model
+            }catch{
+                fatalError(error.localizedDescription)
+            }
+        }else{
+            fatalError("Data unavailable at path \(url.path)")
+        }
+    }
+    
+    static func loadData (_ fileName:String) -> Data? {
+        let url = getDocumentDirectory().appendingPathComponent(fileName, isDirectory: false)
+        if !FileManager.default.fileExists(atPath: url.path) {
+            fatalError("File not found at path \(url.path)")
+        }
+        
+        if let data = FileManager.default.contents(atPath: url.path) {
+            return data
+        }else{
+            fatalError("Data unavailable at path \(url.path)")
+        }
+    }
+    
+    static func loadAll <T:Decodable> (_ type:T.Type) -> [T] {
+        
+        do {
+            let files = try FileManager.default.contentsOfDirectory(atPath: getDocumentDirectory().path)
+            
+            var modelObjects = [T]()
+            for fileName in files {
+                modelObjects.append(load(fileName, with: type))
+            }
+            return modelObjects
+            
+        }catch{
+            fatalError("could not load any files")
+        }
+        
+    }
+    
+}
